f1a43e327e995021d6ae13d3bb7ff977c5094aa2
root <root@localhost.localdomain>
2022-08-26 11:59:54 +0800

    use std::thread

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,23 @@
 #include "player/player.h"
-#include <pthread.h>
-#include <semaphore.h>
+//#include <pthread.h>
+//#include <semaphore.h>
+#include <thread>
 
 int main(int argc, char **argv)
 {
-	sem_t sem;
-	pthread_t tid;
-	sem_init(&sem, 0, 1);
+	//sem_t sem;
+	//pthread_t tid;
+	//sem_init(&sem, 0, 1);
 	Player player;
 	player.question.InitPlayer(&player);
-	pthread_create(&tid, NULL, p_input, &player);
+	//pthread_create(&tid, NULL, p_input, &player);
+	std::thread tr(p_input, &player);
 	while (true)
 	{
 		player.Update();
 	}
-	pthread_join(tid, NULL);
-	sem_destroy(&sem);
+	tr.join();
+	//pthread_join(tid, NULL);
+	//sem_destroy(&sem);
 	return 0;
 }
